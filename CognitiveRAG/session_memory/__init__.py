f71d09fbfc3259e0338ec62f6cbea776d98b19da
Constantin Iliescu <ictinpr@yahoo.com>
2026-03-18 10:13:31 +0100

    session_memory: export store classes from package init for tests

--- CognitiveRAG/session_memory/__init__.py
+++ CognitiveRAG/session_memory/__init__.py
@@ -1,1 +1,16 @@
 # session_memory package init
+from .conversation_store import ConversationStore
+from .message_parts_store import MessagePartsStore
+from .summary_nodes import SummaryNodeStore
+from .summary_edges import SummaryEdgeStore
+from .context_items import ContextItemStore
+from .large_file_store import LargeFileStore
+
+__all__ = [
+    "ConversationStore",
+    "MessagePartsStore",
+    "SummaryNodeStore",
+    "SummaryEdgeStore",
+    "ContextItemStore",
+    "LargeFileStore",
+]
